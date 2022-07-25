f016b9f0549c98490e102b051cfb0aefe04f9c65
EastZero123 <cdy1005p@naver.com>
2022-07-25 19:08:26 +0900

    완

---
+++ Doit/vue-todo/src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput v-on:addTodo="addTodo"></TodoInput>
+    <TodoList v-bind:propsdata="todoItems" @removeTodo="removeTodo"></TodoList>
+    <TodoFooter v-on:removeAll="clearAll"></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoFooterVue from "./components/TodoFooter.vue";
+import TodoHeaderVue from "./components/TodoHeader.vue";
+import TodoInputVue from "./components/TodoInput.vue";
+import TodoListVue from "./components/TodoList.vue";
+
+export default {
+  props: ["propsdata"],
+  data() {
+    return {
+      todoItems: []
+    };
+  },
+  methods: {
+    clearAll() {
+      localStorage.clear();
+      this.todoItems = [];
+    },
+    addTodo(todoItem) {
+      localStorage.setItem(todoItem, todoItem);
+      this.todoItems.push(todoItem);
+    },
+    removeTodo(todoItem, index) {
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index, 1);
+    }
+  },
+  created() {
+    if (localStorage.length > 0) {
+      for (var i = 0; i < localStorage.length; i++) {
+        this.todoItems.push(localStorage.key(i));
+      }
+    }
+  },
+  components: {
+    TodoHeader: TodoHeaderVue,
+    TodoInput: TodoInputVue,
+    TodoList: TodoListVue,
+    TodoFooter: TodoFooterVue
+  }
+};
+</script>
+
+<style>
+body {
+  text-align: center;
+  background-color: #f6f6f8;
+}
+input {
+  border-style: groove;
+  width: 200px;
+}
+button {
+  border-style: groove;
+}
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.3);
+}
+</style>
