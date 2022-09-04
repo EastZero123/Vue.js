c715c41c087cdff70b1085b2f54b36a5cb876d22
EastZero123 <cdy1005p@naver.com>
2022-09-04 21:45:25 +0900

    CH07 finish

--- Udemy/CH07/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
+++ Udemy/CH07/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -1,5 +1,33 @@
-<template></template>
+<template>
+  <li>
+    <h2>{{ friends.name }}</h2>
+    <button @click="toggleDetails">
+      {‌{ detailsAreVisible ? 'Hide' : 'Show' }} Details
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong>{{ friends.phone }}</li>
+      <li><strong>Email:</strong>{{ friends.email }}</li>
+    </ul>
+  </li>
+</template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "manuel",
+        name: "Manuel Lorenz",
+        phone: "0123 45678 90",
+        email: "manuel@localhost.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
 </script>
