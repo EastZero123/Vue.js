b67fe0b79a29d0590deef99d6349e6fa96fdb6e1
EastZero123 <cdy1005p@naver.com>
2023-04-01 12:19:34 +0900

    CH12 finish

--- UdemyKR/CH12/http-01-starting-setup/src/components/survey/UserExperiences.vue
+++ UdemyKR/CH12/http-01-starting-setup/src/components/survey/UserExperiences.vue
@@ -3,9 +3,18 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="loadExperiences"
+          >Load Submitted Experiences</base-button
+        >
       </div>
-      <ul>
+      <p v-if="isLoading">Loading...</p>
+      <p v-else-if="!isLoading && error">
+        {{ error }}
+      </p>
+      <p v-else-if="!isLoading && (!results || results.length === 0)">
+        No stored experiences found. Start adding some survey
+      </p>
+      <ul v-else>
         <survey-result
           v-for="result in results"
           :key="result.id"
@@ -21,9 +30,49 @@
 import SurveyResult from './SurveyResult.vue';
 
 export default {
-  props: ['results'],
   components: {
     SurveyResult,
+  },
+  data() {
+    return {
+      results: [],
+      isLoading: false,
+      error: null,
+    };
+  },
+  methods: {
+    loadExperiences() {
+      this.isLoading = true;
+      this.error = null;
+      fetch(
+        'https://netflix-clone-1b800-default-rtdb.firebaseio.com/surveys.json'
+      )
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+        })
+        .then((data) => {
+          this.isLoading = false;
+          const results = [];
+          for (const id in data) {
+            results.push({
+              id: id,
+              name: data[id].name,
+              rating: data[id].rating,
+            });
+          }
+          this.results = results;
+        })
+        .catch((err) => {
+          console.log(err);
+          this.isLoading = false;
+          this.error = 'Failed error';
+        });
+    },
+  },
+  mounted() {
+    this.loadExperiences();
   },
 };
 </script>
