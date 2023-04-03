75dbc3579c21554f8a0b77a0bfb08520d1b9f421
EastZero123 <99165555+EastZero123@users.noreply.github.com>
2023-04-03 16:53:54 +0900

    CH15 checkpoint

--- UdemyKR/CH15/vuex-01-starting-setup/src/App.vue
+++ UdemyKR/CH15/vuex-01-starting-setup/src/App.vue
@@ -1,13 +1,30 @@
 <template>
-  <base-container title="Vuex"></base-container>
+  <base-container title="Vuex">
+    <the-counter></the-counter>
+    <favorite-value></favorite-value>
+    <button @click="addOne">Add 1</button>
+    <change-counter></change-counter>
+  </base-container>
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
+import TheCounter from './components/TheCounter.vue';
+import ChangeCounter from './components/ChangeCounter.vue';
+import FavoriteValue from './components/FavoriteValue.vue';
 
 export default {
   components: {
     BaseContainer,
+    TheCounter,
+    FavoriteValue,
+    ChangeCounter,
+  },
+
+  methods: {
+    addOne() {
+      this.$store.commit('increase', { value: 10 });
+    },
   },
 };
 </script>
